6f6bcba847afad5733ca4739dee1dd8e324392d0
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-01-15 08:42:23 +0530

    product_category is created and inserted data

--- projectFiles/eCommerce Product Database Design/eCommerce Product Database Design.sql
+++ projectFiles/eCommerce Product Database Design/eCommerce Product Database Design.sql
@@ -26,11 +26,39 @@ create table
         product_categoryId int,
         categoryName varchar(100),
         categoryImage varchar(600),
-        productGenderId int 
+        productGenderId int,
         -- This is the name of the  primary key constraint.
         constraint pk_product_category primary key (product_categoryId),
-
         -- This is the name of the  foreign key constraint.
+        constraint fk_productGenderId foreign key (productGenderId) references product_Gender (product_gender_id)
+    );
 
-        constraint fk_
+
+ -- inserting data to product_category table 
+insert into
+    product_category (
+        product_categoryId,
+        categoryName,
+        categoryImage,
+        productGenderId
     )
+values
+    (1, 'dress','image.png',1),
+    (2, 'coats','image.png',1),
+    (3, 'sports coart','image.png',1),
+    (4, 'gift','image.png',1),
+    (5, 'chudidar','image.png',1),
+    (6, 'sares ','image.png',1),
+    (7, 'jeans  ','image.png',2),
+    (8, 'formals  ','image.png',2),
+    (9, 'shirts ','image.png',2),
+    (11, 'pants ','image.png',2),
+    (12, 'boy shorts ','image.png',2);
+    
+-- fetching all data from product_category
+
+select *  from product_category;
+
+-- fetching data by gender  wise if productGenderId=1 then it will be WOMEN and if productGenderId=2 then it will be MAN
+
+select *  from product_category where productGenderId=1;
